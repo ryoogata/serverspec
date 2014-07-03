0983494c17b27a33028ddf5f6ccb1000759e6ec2
ryoogata <ryo.ogata@gmail.com>
2014-07-03 12:06:55 +0900

    knife の設定ファイルを引数に指定

--- cook.rb
+++ cook.rb
@@ -7,4 +7,4 @@ user =  properties[properties.keys.join][:user]
 port =  properties[properties.keys.join][:port]
 cookbook =  File::basename(Dir::pwd)
 
-system("knife solo cook -p #{port} -i ../ssh.pem #{user}@#{host} #{cookbook}.json")
+system("knife solo cook -c knife.rb -p #{port} -i ../ssh.pem #{user}@#{host} #{cookbook}.json")
--- prepare.rb
+++ prepare.rb
@@ -6,4 +6,4 @@ host = properties.keys.join
 user =  properties[properties.keys.join][:user]
 port =  properties[properties.keys.join][:port]
 
-system("knife solo prepare --bootstrap-version 11.12.4 -p #{port} -i ../ssh.pem #{user}@#{host}")
+system("knife solo prepare -c knife.rb --bootstrap-version 11.12.4 -p #{port} -i ../ssh.pem #{user}@#{host}")
